2cf4ec2ebb82ca5bed78c2560ecec255cacf742b
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2018-06-18 20:31:48 +0100

    Desynchronize IP initialization and DHCP lease obtention

--- client/mirage/dhcp_ipv4.ml
+++ client/mirage/dhcp_ipv4.ml
@@ -5,7 +5,12 @@ module Make(Dhcp_client : DHCP_CLIENT) (Ethif : ETHIF)(Arp : ARP) = struct
   (* for now, just wrap a static ipv4 *)
   module I = Static_ipv4.Make(Ethif)(Arp)
   include I
+
+  let update_ip_settings ip (settings: ipv4_config) = 
+    I.set_config ~ip:settings.address ~network:settings.network ~gateway:settings.gateway ip
+
   let connect dhcp ethif arp =
-    Lwt_stream.last_new dhcp >>= fun (config : ipv4_config) ->
-    I.connect ~ip:config.address ~network:config.network ~gateway:config.gateway ethif arp
+    I.connect ethif arp >>= fun ip ->
+    Lwt.ignore_result (Lwt_stream.iter_s (update_ip_settings ip) dhcp);
+    Lwt.return ip
 end
