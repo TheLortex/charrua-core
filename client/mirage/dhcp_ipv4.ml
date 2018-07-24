c5d8e9a176c47ef296f333f4089456359b7c3507
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2018-07-24 12:00:14 +0100

    A comment on the modification as I won't be able to update this soon enough.

--- client/mirage/dhcp_ipv4.ml
+++ client/mirage/dhcp_ipv4.ml
@@ -9,6 +9,9 @@ module Make(Dhcp_client : DHCP_CLIENT) (Ethif : ETHIF)(Arp : ARP) = struct
   let update_ip_settings ip (settings: ipv4_config) = 
     I.set_config ~ip:settings.address ~network:settings.network ~gateway:settings.gateway ip
 
+  (* Listen to the DHCP update stream and update configured IP according to it.
+   * TODO: Create a Dynamic_IPV4 module for this purpose,
+   * that should propagate changes to upper stack.*)
   let connect dhcp ethif arp =
     I.connect ethif arp >>= fun ip ->
     Lwt.ignore_result (Lwt_stream.iter_s (update_ip_settings ip) dhcp);
